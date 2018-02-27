5b6cdbcf711ca2ff8f5582d41986457eac99705c
KateFlylex <flylex@gmail.com>
2018-02-27 20:45:24 +0200

    task_app added

---
+++ task_app/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = React.createClass({
+	
+		render: function() {
+		return (
+			<div> About</div>
+		);
+	}
+
+});
+
+export default About;
---
+++ task_app/src/components/ItemsApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ItemsPage = React.createClass({
+	
+		render: function() {
+		return (
+			<div> Items App </div>
+		);
+	}
+
+});
+
+export default ItemsPage;
